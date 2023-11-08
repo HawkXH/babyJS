e7c3029a08452b25b1a16d77cb6ad63024959684
sapphire <hawkxh@gmail.com>
2023-11-07 21:22:17 -0500

    Slightly better water

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Import Babylon.js modules
 import * as BABYLON from 'babylonjs';
 import 'babylonjs-loaders';
+import 'babylonjs-materials'; // Import the materials module
 
 document.addEventListener("DOMContentLoaded", function () {
     var canvas = document.getElementById("renderCanvas"); // Explicitly specify the type
@@ -67,7 +68,97 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     var angle = 0;
+// Create a water material using ShaderMaterial
+    var waterMaterial = new BABYLON.ShaderMaterial("waterMaterial", scene, {
+            vertex: "water",
+            fragment: "water",
+        },
+        {
+            attributes: ["position", "normal", "uv"],
+            uniforms: ["world", "worldView", "worldViewProjection", "view", "projection", "time", "blurAmount"],
+        });
 
+// Set the water material properties
+    waterMaterial.setTexture("diffuseTexture", new BABYLON.Texture("https://media.istockphoto.com/id/1300107681/photo/surface-of-the-atlantic-ocean.jpg?s=612x612&w=0&k=20&c=w_5KDlGHRBKgLzjyA6KrdFHDlrO4B2M4LtflWAyzGhc=", scene));
+    waterMaterial.setFloat("uScale", 50); // Adjust the tiling as needed
+    waterMaterial.setFloat("vScale", 50);
+    waterMaterial.setVector3("specularColor", new BABYLON.Vector3(0, 0, 0)); // No specular reflection for water
+    waterMaterial.setVector3("emissiveColor", new BABYLON.Vector3(1, 1, 1)); // Adjust emissive color as needed
+
+    waterMaterial.setFloat("windForce", 10);
+    waterMaterial.setVector2("windDirection", new BABYLON.Vector2(1, 0));
+    waterMaterial.setColor3("waterColor", new BABYLON.Color3(0.0, 0.3, 0.6));
+    waterMaterial.setFloat("colorBlendFactor", 0.0);
+    waterMaterial.setFloat("bumpHeight", 0.1);
+    waterMaterial.setFloat("waveHeight", 5);
+
+    waterMaterial.setFloat("time", 0);
+    waterMaterial.setFloat("blurAmount", 0.01); // Adjust the blur amount as needed
+
+// Create a water mesh (plane)
+    var waterMesh = BABYLON.Mesh.CreateGround("waterMesh", 1000, 1000, 32, scene, false);
+    waterMesh.material = waterMaterial;
+
+// Load water shader code
+    BABYLON.Effect.ShadersStore["waterVertexShader"] = `
+    precision highp float;
+    attribute vec3 position;
+    attribute vec3 normal;
+    attribute vec2 uv;
+
+    uniform mat4 world;
+    uniform mat4 worldView;
+    uniform mat4 worldViewProjection;
+    uniform mat4 view;
+    uniform mat4 projection;
+
+    varying vec3 vPosition;
+    varying vec3 vNormal;
+    varying vec2 vUV;
+
+    void main(void) {
+        vPosition = position;
+        vNormal = normal;
+        vUV = uv;
+        gl_Position = worldViewProjection * vec4(position, 1.0);
+    }
+`;
+
+    BABYLON.Effect.ShadersStore["waterFragmentShader"] = `
+    precision highp float;
+
+    varying vec3 vPosition;
+    varying vec3 vNormal;
+    varying vec2 vUV;
+
+    uniform sampler2D diffuseTexture;
+    uniform float uScale;
+    uniform float vScale;
+    uniform vec3 specularColor;
+    uniform vec3 emissiveColor;
+    uniform float windForce;
+    uniform vec2 windDirection;
+    uniform vec3 waterColor;
+    uniform float colorBlendFactor;
+    uniform float bumpHeight;
+    uniform float waveHeight;
+    uniform float time;
+    uniform float blurAmount;
+
+    void main(void) {
+        // Apply blur effect
+        vec2 uv = vUV;
+        uv += blurAmount * (sin(time) + cos(time)) * vec2(1.0, -1.0);
+
+        // Sample the texture
+        vec4 baseColor = texture2D(diffuseTexture, uv * vec2(uScale, vScale));
+
+        // ... (rest of the water shader code)
+
+        // Output final color
+        gl_FragColor = baseColor;
+    }
+`;
     // set background color to clear
     scene.clearColor = new BABYLON.Color4(0, 0, 0, 0);
 
